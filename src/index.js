fd7b79ea3299eb65c74f6d6add64d2335d68f578
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-03-16 19:33:16 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { PORT } from './config/serverConfig.js';
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'pong' });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port 3000');
+});
