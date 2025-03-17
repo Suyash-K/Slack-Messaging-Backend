2e8fdb797462ee7ec80f94ff21593a6224471c58
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-03-18 01:45:58 +0530

    Db config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
 
 const app = express();
+
+app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/ping', (req, res) => {
   res.status(StatusCodes.OK).json({ message: 'pong' });
@@ -11,4 +16,5 @@ app.get('/ping', (req, res) => {
 
 app.listen(PORT, () => {
   console.log('Server is running on port 3000');
+  connectDB();
 });
