0df578bee876eb2a634d48b712a6ce234d9d1ef3
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-03-19 21:46:55 +0530

    sign in and jwt token generated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 
-import { signUpService } from '../services/userService.js';
+import {signInService, signUpService } from '../services/userService.js';
 import {
   customErrorResponse,
   internalErrorResponse,
@@ -24,4 +24,22 @@ export const signUp = async (req, res) => {
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(internalErrorResponse(error));
   }
+};
+
+export const signIn = async (req, res) => {
+    try {
+      const response = await signInService(req.body);
+      return res
+        .status(StatusCodes.OK)
+        .json(successResponse(response, 'User signed in successfully'));
+    } catch (error) {
+      console.log('User controller error', error);
+      if (error.statusCode) {
+        return res.status(error.statusCode).json(customErrorResponse(error));
+      }
+  
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json(internalErrorResponse(error));
+    }
 };
