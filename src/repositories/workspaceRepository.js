0e93850ddef198954500dcc2df8d7c7b5360cb3b
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-03-21 21:14:16 +0530

    added multiple workspace services

--- src/repositories/workspaceRepository.js
+++ src/repositories/workspaceRepository.js
@@ -8,6 +8,13 @@ import crudRepository from './crudRepository.js';
 
 const workspaceRepository = {
   ...crudRepository(Workspace),
+  getWorkspaceDetailsById: async function (workspaceId) {
+    const workspace = await Workspace.findById(workspaceId)
+      .populate('members.memberId', 'username email avatar')
+      .populate('channels');
+
+    return workspace;
+  },
   getWorkspaceByName: async function (workspaceName) {
     const workspace = await Workspace.findOne({
       name: workspaceName
@@ -103,7 +110,10 @@ const workspaceRepository = {
       });
     }
 
-    const channel = await channelRepository.create({ name: channelName });
+    const channel = await channelRepository.create({
+      name: channelName,
+      workspaceId: workspaceId
+    });
 
     workspace.channels.push(channel);
     await workspace.save();
