991b738f460d7fe9cf52e45a4ad69e1af09cbc3c
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-03-22 20:29:40 +0530

    added redis ui using bullboard

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import bullServerAdapter from './config/bullBoardConfig.js';
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
@@ -8,9 +9,11 @@ import apiRouter from './routes/apiRoutes.js';
 const app = express();
 
 app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
- app.use('/api', apiRouter);
+app.use('/ui', bullServerAdapter.getRouter());
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   res.status(StatusCodes.OK).json({ message: 'pong' });
